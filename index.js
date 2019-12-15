8109a57c0464ad1f8daec36091a40f88a17da6a5
Jordan Ryan Moore <jordanryanmoore@me.com>
2019-12-15 08:54:49 -0800

    Make modes and location configurable. Move Pusher initialization from ScoutPlatform to ScoutApi.

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const ScoutApi = require('./lib/scout-api');
 
 module.exports = (homebridge) => {
     homebridge.registerPlatform('homebridge-scout', 'ScoutAlarm', function(logger, config) {
-        let api = new ScoutApi(logger, config);
+        let api = new ScoutApi(logger, config.auth.email, config.auth.password);
 
-        return new ScoutPlatform(homebridge, logger, api);
+        return new ScoutPlatform(homebridge, logger, api, config.location, config.modes);
     });
 };
