b192f66e8e0237622f6c39fb7838be60b13d7deb
Jordan Ryan Moore <jordanryanmoore@me.com>
2020-02-14 20:53:54 -0800

    Add minimal tests for TemperatureSensorServiceFactory.

--- tst/serviceFactory/sensor/temperatureSensorServiceFactory.spec.ts
+++ tst/serviceFactory/sensor/temperatureSensorServiceFactory.spec.ts
@@ -1,3 +1,94 @@
+import { DeviceType } from "scout-api";
+import { AccessoryContext } from "../../../src/accessoryFactory";
+import { SensorAccessoryContext } from "../../../src/accessoryFactory/sensorAccessoryFactory";
+import { HomebridgeContext, ScoutContext } from "../../../src/context";
 import { TemperatureSensorServiceFactory } from "../../../src/serviceFactory/sensor/temperatureSensorServiceFactory";
+import { ServiceConstructor, Service, CharacteristicConstructor, Characteristic, CharacteristicValue } from "../../../src/types";
 
-test.todo(`${TemperatureSensorServiceFactory.name}`);
+describe(`${TemperatureSensorServiceFactory.name}`, () => {
+    let homebridge: HomebridgeContext;
+    let scout: ScoutContext;
+    let context: AccessoryContext<SensorAccessoryContext>;
+    let serviceFactory: TemperatureSensorServiceFactory;
+
+    beforeEach(() => {
+        homebridge = {
+            api: {
+                hap: {
+                    Characteristic: {
+                        StatusFault: (jest.fn() as unknown) as CharacteristicConstructor<unknown>,
+                        StatusLowBattery: (jest.fn() as unknown) as CharacteristicConstructor<unknown>,
+                        CurrentTemperature: (jest.fn() as unknown) as CharacteristicConstructor<unknown>,
+                    },
+                    Service: {
+                        TemperatureSensor: (jest.fn() as unknown) as ServiceConstructor,
+                    },
+                },
+            },
+        } as HomebridgeContext;
+
+        context = {
+            custom: {
+                device: {
+                    type: DeviceType.DoorPanel,
+                    reported: {
+                        temperature: {
+                            degrees: 12.3,
+                        },
+                    },
+                },
+            },
+        } as AccessoryContext<SensorAccessoryContext>;
+
+        serviceFactory = new TemperatureSensorServiceFactory(homebridge, scout);
+    });
+
+    describe(".getService()", () => {
+        test("without temperature", () => {
+            delete context.custom.device.reported!.temperature;
+
+            expect(serviceFactory.getService(context)).toBeUndefined();
+        });
+
+        test("with temperature", () => {
+            expect(serviceFactory.getService(context)).toStrictEqual(homebridge.api.hap.Service.TemperatureSensor);
+        });
+    });
+
+    describe(".configureService()", () => {
+        let service: Service;
+        let updatedCharacteristics: Map<CharacteristicConstructor<unknown>, CharacteristicValue>;
+
+        beforeEach(() => {
+            service = {
+                getCharacteristic: jest.fn() as unknown,
+            } as Service;
+
+            updatedCharacteristics = new Map();
+
+            (service.getCharacteristic as jest.Mock<Characteristic>).mockImplementation((type: CharacteristicConstructor<unknown>) => {
+                return {
+                    updateValue: jest.fn().mockImplementation((value: CharacteristicValue) => {
+                        updatedCharacteristics.set(type, value);
+                    }) as unknown,
+                } as Characteristic;
+            });
+        });
+
+        test("smoke alarm", () => {
+            context.custom.device.type = DeviceType.SmokeAlarm;
+
+            serviceFactory.configureService(service, context);
+
+            expect(updatedCharacteristics.get(homebridge.api.hap.Characteristic.CurrentTemperature)).toBeUndefined();
+        });
+
+        test("other device type", () => {
+            serviceFactory.configureService(service, context);
+
+            expect(updatedCharacteristics.get(homebridge.api.hap.Characteristic.CurrentTemperature)).toEqual(
+                context.custom.device.reported?.temperature?.degrees,
+            );
+        });
+    });
+});
