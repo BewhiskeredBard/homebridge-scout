6dc184bcb6e7e546d65c98d2977a07912725f216
Jordan Ryan Moore <jordanryanmoore@me.com>
2020-02-15 09:03:26 -0800

    Add some basic tests for SecuritySystemServiceFactory.

--- tst/mocks.ts
+++ tst/mocks.ts
@@ -13,6 +13,8 @@ export function mockHomebridgeContext(): HomebridgeContext {
                     CurrentTemperature: (jest.fn() as unknown) as CharacteristicConstructor<unknown>,
                     LeakDetected: (jest.fn() as unknown) as CharacteristicConstructor<unknown>,
                     MotionDetected: (jest.fn() as unknown) as CharacteristicConstructor<unknown>,
+                    SecuritySystemCurrentState: (jest.fn() as unknown) as CharacteristicConstructor<unknown>,
+                    SecuritySystemTargetState: (jest.fn() as unknown) as CharacteristicConstructor<unknown>,
                     SmokeDetected: (jest.fn() as unknown) as CharacteristicConstructor<unknown>,
                     StatusFault: (jest.fn() as unknown) as CharacteristicConstructor<unknown>,
                     StatusLowBattery: (jest.fn() as unknown) as CharacteristicConstructor<unknown>,
@@ -23,6 +25,7 @@ export function mockHomebridgeContext(): HomebridgeContext {
                     HumiditySensor: (jest.fn() as unknown) as ServiceConstructor,
                     LeakSensor: (jest.fn() as unknown) as ServiceConstructor,
                     MotionSensor: (jest.fn() as unknown) as ServiceConstructor,
+                    SecuritySystem: (jest.fn() as unknown) as ServiceConstructor,
                     SmokeSensor: (jest.fn() as unknown) as ServiceConstructor,
                     TemperatureSensor: (jest.fn() as unknown) as ServiceConstructor,
                 },
@@ -39,6 +42,17 @@ export function mockHomebridgeContext(): HomebridgeContext {
 
     homebridge.api.hap.Characteristic.LeakDetected.LEAK_DETECTED = 123;
     homebridge.api.hap.Characteristic.LeakDetected.LEAK_NOT_DETECTED = 234;
+
+    homebridge.api.hap.Characteristic.SecuritySystemCurrentState.ALARM_TRIGGERED = 123;
+    homebridge.api.hap.Characteristic.SecuritySystemCurrentState.AWAY_ARM = 234;
+    homebridge.api.hap.Characteristic.SecuritySystemCurrentState.DISARMED = 345;
+    homebridge.api.hap.Characteristic.SecuritySystemCurrentState.NIGHT_ARM = 456;
+    homebridge.api.hap.Characteristic.SecuritySystemCurrentState.STAY_ARM = 567;
+
+    homebridge.api.hap.Characteristic.SecuritySystemTargetState.AWAY_ARM = 123;
+    homebridge.api.hap.Characteristic.SecuritySystemTargetState.DISARM = 234;
+    homebridge.api.hap.Characteristic.SecuritySystemTargetState.NIGHT_ARM = 345;
+    homebridge.api.hap.Characteristic.SecuritySystemTargetState.STAY_ARM = 456;
 
     homebridge.api.hap.Characteristic.SmokeDetected.SMOKE_DETECTED = 123;
     homebridge.api.hap.Characteristic.SmokeDetected.SMOKE_NOT_DETECTED = 234;
--- tst/serviceFactory/hub/securitySystemServiceFactory.spec.ts
+++ tst/serviceFactory/hub/securitySystemServiceFactory.spec.ts
@@ -1,3 +1,80 @@
+import { AccessoryContext } from "../../../src/accessoryFactory";
+import { SecuritySystemContext } from "../../../src/accessoryFactory/securitySystemAccessoryFactory";
+import { HomebridgeContext, ScoutContext } from "../../../src/context";
 import { SecuritySystemServiceFactory } from "../../../src/serviceFactory/hub/securitySystemServiceFactory";
+import { Service, CharacteristicConstructor, Characteristic, CharacteristicValue } from "../../../src/types";
+import * as mocks from "../../mocks";
+import { Mode } from "scout-api";
 
-test.todo(`${SecuritySystemServiceFactory.name}`);
+describe(`${SecuritySystemServiceFactory.name}`, () => {
+    let homebridge: HomebridgeContext;
+    let scout: ScoutContext;
+    let context: AccessoryContext<SecuritySystemContext>;
+    let serviceFactory: SecuritySystemServiceFactory;
+
+    beforeEach(() => {
+        homebridge = mocks.mockHomebridgeContext();
+
+        context = {
+            custom: {
+                hub: {},
+                modes: [] as Array<Mode>,
+            },
+        } as AccessoryContext<SecuritySystemContext>;
+
+        serviceFactory = new SecuritySystemServiceFactory(homebridge, scout);
+    });
+
+    describe(".getService()", () => {
+        test("without configured modes", () => {
+            expect(serviceFactory.getService(context)).toBeUndefined();
+        });
+
+        test("with configured modes", () => {
+            homebridge.config.modes = {
+                away: ["mode1"],
+                night: ["mode2"],
+                stay: ["mode3"],
+            };
+
+            context.custom.modes = [
+                {
+                    name: "mode1",
+                },
+                {
+                    name: "mode2",
+                },
+                {
+                    name: "mode3",
+                },
+            ] as Array<Mode>;
+
+            expect(serviceFactory.getService(context)).toStrictEqual(homebridge.api.hap.Service.SecuritySystem);
+        });
+
+        test.todo("with invalid configured modes");
+    });
+
+    describe(".configureService()", () => {
+        let service: Service;
+        let updatedCharacteristics: Map<CharacteristicConstructor<unknown>, CharacteristicValue>;
+
+        beforeEach(() => {
+            service = {
+                getCharacteristic: jest.fn() as unknown,
+            } as Service;
+
+            updatedCharacteristics = new Map();
+
+            (service.getCharacteristic as jest.Mock<Characteristic>).mockImplementation((type: CharacteristicConstructor<unknown>) => {
+                return {
+                    updateValue: jest.fn().mockImplementation((value: CharacteristicValue) => {
+                        updatedCharacteristics.set(type, value);
+                    }) as unknown,
+                } as Characteristic;
+            });
+        });
+
+        test.todo("so many use cases…");
+    });
+});
