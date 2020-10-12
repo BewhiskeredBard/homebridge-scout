BewhiskeredBard/homebridge-scout

commit 79c97453432de55a44fb36f6b047e98f7d092aa9

Stop using dynamic require()'s for JSON (resolves #100).

src/scoutPlatformPlugin.ts
@@ -1,4 +1,5 @@
 import type { DynamicPlatformPlugin, Logging, PlatformConfig, API, PlatformAccessory } from 'homebridge';
+import * as packageConfig from '../package.json';
 import { SecuritySystemAccessoryFactory } from './accessoryFactory/securitySystemAccessoryFactory';
 import { SensorAccessoryFactory } from './accessoryFactory/sensorAccessoryFactory';
 import { HomebridgeContextFactory, ScoutContextFactory } from './context';
@@ -20,9 +21,7 @@ export class ScoutPlatformPlugin implements DynamicPlatformPlugin {
     private readonly orchestrator: Orchestrator;
 
     public constructor(logger: Logging, config: PlatformConfig, api: API) {
-        // TODO: Replace `require` (see https://github.com/jordanryanmoore/homebridge-scout/issues/100).
-        // eslint-disable-next-line @typescript-eslint/no-var-requires
-        const pluginVersion = (require('../package.json') as Record<string, unknown>).version as string;
+        const pluginVersion = (packageConfig as Record<string, unknown>).version as string;
 
         logger.info(`Running ${config.platform}-${pluginVersion} on homebridge-${api.serverVersion}.`);
 

src/context/homebridge.ts
@@ -1,5 +1,6 @@
 import * as Ajv from 'ajv';
 import type { API, Logging, PlatformConfig } from 'homebridge';
+import * as schema from '../../config.schema.json';
 
 export enum HomebridgeConfigMode {
     Stay = 'stay',
@@ -27,14 +28,10 @@ export interface HomebridgeContext {
 }
 
 export class HomebridgeContextFactory {
-    private static readonly JSON_SCHEMA_PATH = '../../config.schema.json';
-
     private readonly schema: Record<string, unknown>;
 
     public constructor() {
-        // TODO: Replace `require` (see https://github.com/jordanryanmoore/homebridge-scout/issues/100).
-        // eslint-disable-next-line @typescript-eslint/no-var-requires
-        this.schema = (require(HomebridgeContextFactory.JSON_SCHEMA_PATH) as Record<string, unknown>).schema as Record<string, unknown>;
+        this.schema = (schema as Record<string, unknown>).schema as Record<string, unknown>;
     }
 
     public create(api: API, logger: Logging, config: unknown): HomebridgeContext {

commit f42b6facb8a84d4f62e0287f8b4a948a2a4efc87

Change how the package version is read to fix an unintentional change in the TypeScript-generated lib structure.
This fixes an issue introduced in 79c97453432de55a44fb36f6b047e98f7d092aa9.

src/scoutPlatformPlugin.ts
@@ -1,5 +1,5 @@
+import * as fs from 'fs';
 import type { DynamicPlatformPlugin, Logging, PlatformConfig, API, PlatformAccessory } from 'homebridge';
-import * as packageConfig from '../package.json';
 import { SecuritySystemAccessoryFactory } from './accessoryFactory/securitySystemAccessoryFactory';
 import { SensorAccessoryFactory } from './accessoryFactory/sensorAccessoryFactory';
 import { HomebridgeContextFactory, ScoutContextFactory } from './context';
@@ -21,7 +21,7 @@ export class ScoutPlatformPlugin implements DynamicPlatformPlugin {
     private readonly orchestrator: Orchestrator;
 
     public constructor(logger: Logging, config: PlatformConfig, api: API) {
-        const pluginVersion = (packageConfig as Record<string, unknown>).version as string;
+        const pluginVersion = (JSON.parse(fs.readFileSync(require.resolve('../package.json'), 'utf8')) as Record<string, unknown>).version as string;
 
         logger.info(`Running ${config.platform}-${pluginVersion} on homebridge-${api.serverVersion}.`);
 